d1de606478932191080a1e2319baa6e6e01582e8
ozy <ozyftw17@gmail.com>
2024-06-01 02:12:41 +0530

    clerk init

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,14 @@
+const TopNav = () => {
+  return (
+    <nav className="item-center flex w-full justify-between border-b p-4">
+      <div>
+        <h1 className="text-2xl font-bold">Image Gallery</h1>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">Sign in</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default TopNav;
