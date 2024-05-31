dbf818dd120ac17c70f202c52c207098d455e910
ozy <ozyftw17@gmail.com>
2024-06-01 01:38:35 +0530

    db init done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { db } from "~/server/db";
 const mockUrls = [
   "https://utfs.io/f/468be4de-f8cc-4363-91b8-8fd16fe7ccb7-1xcfdl.png",
   "https://utfs.io/f/abb93033-624c-48ba-9873-4b33b672769e-1qer2i.png",
@@ -9,10 +10,14 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
-    <div className="overflow-hidden">
+    <div className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div
             key={image.id}
