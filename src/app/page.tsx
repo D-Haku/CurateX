b81b5afe648cff4a337c285f4876ad1c7e12393b
ozy <ozyftw17@gmail.com>
2024-06-05 02:55:34 +0530

    ui somewhat fixed🐊

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,21 @@ import Image from "next/image";
 import { db } from "~/server/db";
 import { headers } from "next/headers";
 import { SignedOut, SignedIn } from "@clerk/nextjs";
+import { DirectionAwareHover } from "./_components/direction-aware-hover";
 import { getMyImages } from "~/server/queries";
-
+import { WobbleCard } from "./_components/wobble-card";
 async function Images() {
   const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap  justify-center gap-4 p-4">
-      {[...images, ...images, ...images].map((image) => (
-        <div
-          key={image.id}
-          className="w-48 rounded-xl border-4 border-white p-2"
-        >
+      {[...images, ...images].map((image) => (
+        <div key={image.id} className=" rounded-xl border-4 border-white p-2">
           <Link href={`/img/${image.id}`}>
-            <Image
-              className="rounded-md"
-              src={image.url}
-              style={{ objectFit: "contain" }}
-              width={192}
-              height={192}
-              alt={image.name}
-            />
+            <DirectionAwareHover imageUrl={image.url}>
+              <p className="text-xl font-bold">{image.name}</p>
+            </DirectionAwareHover>
           </Link>
-          <div className="text-white">{image.name}</div>
         </div>
       ))}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,12 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
 
 export default {
   content: ["./src/**/*.tsx"],
@@ -10,5 +17,16 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
